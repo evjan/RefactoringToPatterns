14ab67ad55a5291e23e0a58aadd5cbe88a613df7
Peter Evjan <hello@peterevjan.com>
2019-11-23 16:18:27 +1100

    Simulating multiple clients

--- ExtractComposite/spec/making_a_cake_spec.rb
+++ ExtractComposite/spec/making_a_cake_spec.rb
@@ -6,3 +6,10 @@ describe 'Making a cake' do
     expect(make_cake.calculate_duration).to eq 46
   end
 end
+
+describe 'Making batter' do
+  it 'has the right time consumption' do
+    make_batter = MakeBatter.new
+    expect(make_batter.duration).to eq 4
+  end
+end
