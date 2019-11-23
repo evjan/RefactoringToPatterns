132d76950770e666803df0f8a28de2ef54ddc106
Peter Evjan <hello@peterevjan.com>
2019-11-23 16:16:59 +1100

    A better after

--- ExtractComposite/lib/after.rb
+++ ExtractComposite/lib/after.rb
@@ -1,50 +1,48 @@
-class Task
+class BakeTask
   def add_sub_task(task)
     @sub_tasks ||= []
     @sub_tasks << task
   end
 
   def time_required
-    time = 0
-    @sub_tasks.each {|t| time += t.time_required}
-    time
+    @sub_tasks.map(&:time_required).reduce(:+)
   end
 end
 
-class MakeCake < Task
+class MakeCake < BakeTask
   def initialize
-    add_sub_task(MakeBatter.new)
-    add_sub_task(FillPan.new)
-    add_sub_task(Bake.new)
+    add_sub_task MakeBatter.new
+    add_sub_task FillPan.new
+    add_sub_task Bake.new
   end
 end
 
-class MakeBatter < Task
+class MakeBatter < BakeTask
   def initialize
     add_sub_task(AddIngredients.new)
     add_sub_task(Mix.new)
   end
 end
 
-class AddIngredients < Task
+class AddIngredients < BakeTask
   def time_required
     1
   end
 end
 
-class Mix < Task
+class Mix < BakeTask
   def time_required
     3
   end
 end
 
-class FillPan < Task
+class FillPan < BakeTask
   def time_required
     2
   end
 end
 
-class Bake < Task
+class Bake < BakeTask
   def time_required
     40
   end
