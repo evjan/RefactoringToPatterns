215ccc48207e627bb4dd0fdc26af07f7ccbe52d4
Jason Ona <jason.ona@cogent.co>
2020-10-06 11:14:46 +1100

    Add ComposeMethod practice

---
+++ ComposeMethod/src/actions/types.ts
@@ -0,0 +1,7 @@
+class HomeTypes {
+  static readonly HOME_DATA_UPDATING = 'HOME_DATA_UPDATING'
+  static readonly HOME_DATA_UPDATED = 'HOME_DATA_UPDATED'
+  static readonly HOME_DATA_FAILED_TO_UPDATE = 'HOME_DATA_FAILED_TO_UPDATE'
+}
+
+export {HomeTypes}
